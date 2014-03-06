0587eeae795b32d8bb9049e865d1f5bf97778506
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-06 14:21:58 -0500

    Create TDM.R

---
+++ TDM.R
@@ -0,0 +1,41 @@
+
+
+
+library(tm)
+library(foreign)
+setwd("C:/tales")
+text <- system.file("texts", "txt", package="tm")
+# read in corpus
+corpus <- Corpus(DirSource())
+print(corpus[[13]])
+###############################################
+# use these to remove fine grained distinctions
+###############################################
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, stemDocument)
+print(corpus[[13]])
+
+tdm <- TermDocumentMatrix(corpus,
+                          control=list(weighting = weightBin,
+                                         stopwords = TRUE))
+#inspect
+tdm$dimnames
+# create more dense matrix
+tdm.dense <- removeSparseTerms(dtm, .2)
+
+# write to arff
+
+write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff") # where file is your destination
+
+# or 
+###
+Once you get to this point, you can browse through my code samples and plug in the dtm
+
+recommended libraries:
+library(topicmodels)
+library(lsa)
+library(RTextTools)
+
