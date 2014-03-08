b3154d52413dd76d92973bab4952998a1165bfc1
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-08 00:44:29 -0500

    Update TDM.R

--- TDM.R
+++ TDM.R
@@ -1,7 +1,6 @@
-
-
 library(tm)
 library(foreign)
+#setwd("/home/kingfish/tales")
 setwd("C:/tales")
 text <- system.file("texts", "txt", package="tm")
 # read in corpus
@@ -13,13 +12,13 @@ print(corpus[[13]])
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, removeNumbers)
 corpus <- tm_map(corpus, tolower)
-corpus <- tm_map(corpus, stripWhitespace)
+#corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, stemDocument)
 print(corpus[[13]])
 
-tdm <- TermDocumentMatrix(corpus,
+tdm <- DocumentTermMatrix(corpus,
                           control=list(weighting = weightBin,
-                                         stopwords = TRUE))
+                                       stopwords = TRUE))
 #inspect
 tdm$dimnames
 # create more dense matrix
@@ -27,14 +26,13 @@ tdm.dense <- removeSparseTerms(tdm, .2)
 
 # write to arff
 
-write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff") # where file is your destination
+write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff", relation=tdm.dense$dimnames$Docs) # where file is your destination
 
-# or 
+# or
 ###
-Once you get to this point, you can browse through my code samples and plug in the dtm
+#Once you get to this point, you can browse through my code samples and plug in the dtm
 
-recommended libraries:
-library(topicmodels)
+#recommended libraries:
+  library(topicmodels)
 library(lsa)
 library(RTextTools)
-
