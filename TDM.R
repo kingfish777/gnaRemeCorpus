bccb16f33a68c6ade71e726f32c5a9eb61959c88
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-09 11:35:35 -0400

    Update TDM.R

--- TDM.R
+++ TDM.R
@@ -36,3 +36,56 @@ write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff", relation=tdm.den
   library(topicmodels)
 library(lsa)
 library(RTextTools)
+
+
+
+#############################
+
+
+
+
+library(tm)
+library(foreign)
+library(tau)
+setwd("/home/kingfish/tales")
+text <- system.file("texts", "txt", package="tm")
+# read in corpus
+corpus <- Corpus(DirSource())
+print(corpus[[13]])
+###############################################
+# use these to remove fine grained distinctions
+###############################################
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, tolower)
+#corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, stemDocument)
+tokenize_ngrams <- function(x, n=3) return(rownames(as.data.frame(unclass(textcnt(x,method="string",n=n)))))
+print(corpus[[13]])
+#tokenize_ngrams <- function(x, n=3) return(rownames(as.data.frame(unclass(textcnt(x,method="string",n=n)))))
+
+
+#DocumentTermMatrix(x=corpus, control=())
+
+
+tdm <- DocumentTermMatrix(corpus,
+                          control=list(weighting = weightBin,
+                                       tokenize=tokenize_ngrams,
+                                       stopwords = TRUE))
+#inspect
+tdm$dimnames
+# create more dense matrix
+tdm.dense <- removeSparseTerms(tdm, .2)
+
+# write to arff
+
+write.arff(as.matrix(tdm), file="/home/kingfish/Desktop/term_document_matrix.arff", relation=tdm.dense$dimnames$Docs) # where file is your destination
+
+# or
+###
+#Once you get to this point, you can browse through my code samples and plug in the dtm
+
+#recommended libraries:
+  library(topicmodels)
+library(lsa)
+library(RTextTools)
