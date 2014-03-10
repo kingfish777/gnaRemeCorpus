87ce8129b6c45cb0e16c1cb5c3ce70accfdffab5
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-10 12:23:23 -0400

    Update TDM.R

--- TDM.R
+++ TDM.R
@@ -1,52 +1,7 @@
 library(tm)
 library(foreign)
-#setwd("/home/kingfish/tales")
-setwd("C:/tales")
-text <- system.file("texts", "txt", package="tm")
-# read in corpus
-corpus <- Corpus(DirSource())
-print(corpus[[13]])
-###############################################
-# use these to remove fine grained distinctions
-###############################################
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, tolower)
-#corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, stemDocument)
-print(corpus[[13]])
-#change to TermDocumentMatrix, depending how frustrated you are
-tdm <- DocumentTermMatrix(corpus,
-                          control=list(weighting = weightBin,
-                                       stopwords = TRUE))
-#inspect
-tdm$dimnames
-# create more dense matrix
-tdm.dense <- removeSparseTerms(tdm, .2)
-
-# write to arff
-
-write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff", relation=tdm.dense$dimnames$Docs) # where file is your destination
-#keep fighting the stupid
-# or
-###
-#Once you get to this point, you can browse through my code samples and plug in the dtm
-
-#recommended libraries:
-  library(topicmodels)
-library(lsa)
-library(RTextTools)
-
-
-
-#############################
-
-
-
-
-library(tm)
-library(foreign)
 library(tau)
+#setwd("C:/tales")
 setwd("/home/kingfish/tales")
 text <- system.file("texts", "txt", package="tm")
 # read in corpus
@@ -63,11 +18,7 @@ corpus <- tm_map(corpus, stemDocument)
 tokenize_ngrams <- function(x, n=3) return(rownames(as.data.frame(unclass(textcnt(x,method="string",n=n)))))
 print(corpus[[13]])
 #tokenize_ngrams <- function(x, n=3) return(rownames(as.data.frame(unclass(textcnt(x,method="string",n=n)))))
-
-
 #DocumentTermMatrix(x=corpus, control=())
-
-
 tdm <- DocumentTermMatrix(corpus,
                           control=list(weighting = weightBin,
                                        tokenize=tokenize_ngrams,
@@ -76,16 +27,13 @@ tdm <- DocumentTermMatrix(corpus,
 tdm$dimnames
 # create more dense matrix
 tdm.dense <- removeSparseTerms(tdm, .2)
-
 # write to arff
-
 write.arff(as.matrix(tdm), file="/home/kingfish/Desktop/term_document_matrix.arff", relation=tdm.dense$dimnames$Docs) # where file is your destination
-
+#write.arff(as.matrix(tdm), file="C:/term_document_matrix.arff", relation=tdm.dense$dimnames$Docs) # where file is your destination
 # or
 ###
 #Once you get to this point, you can browse through my code samples and plug in the dtm
-
 #recommended libraries:
-  library(topicmodels)
+library(topicmodels)
 library(lsa)
 library(RTextTools)
