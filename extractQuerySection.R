6d66aa437be1911665d1b073ef60d64c4b21823b
Kingfish Threesevens <scott.malec@gmail.com>
2014-03-17 20:10:31 -0400

    Update extractQuerySection.R

--- extractQuerySection.R
+++ extractQuerySection.R
@@ -17,16 +17,12 @@ convert_text_to_sentences <- function(text, lang = "en") {
   # Function to compute sentence annotations using the Apache OpenNLP Maxent sentence
   # detector employing the default model for language 'en'.
   sentence_token_annotator <- Maxent_Sent_Token_Annotator(language = lang)
-  
   # Convert text to class String from package NLP
   text <- as.String(text)
-  
   # Sentence boundaries in text
   sentence.boundaries <- annotate(text, sentence_token_annotator)
-  
   # Extract sentences
   sentences <- text[sentence.boundaries]
-  
   # return sentences
   return(sentences)
 }
@@ -36,11 +32,9 @@ sentences <- lapply(corpus[[13]], convert_text_to_sentences)
 reshape_corpus <- function(corpus, FUN, ...) {
   # Extract the text from each document in the corpus and put into a list
   text <- lapply(corpus, Content)
-  
   # Basically convert the text
   docs <- lapply(text, FUN, ...)
   docs <- as.vector(unlist(docs))
-  
   # Create a new corpus structure and return it
   new.corpus <- Corpus(VectorSource(docs))
   return(new.corpus)
